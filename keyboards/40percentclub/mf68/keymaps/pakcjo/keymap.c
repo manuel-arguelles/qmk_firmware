6ab00a546c0886362009503d40a3138909fabfbc
Manuel Argüelles <manuel.arguelles@gmail.com>
2020-03-18 20:35:50 -0500

    Modify Pakcjo's mf68 keymap

--- keyboards/40percentclub/mf68/keymaps/pakcjo/keymap.c
+++ keyboards/40percentclub/mf68/keymaps/pakcjo/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MO(1),   KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL,          KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FN1] = LAYOUT_68_ansi(
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,          KC_VOLU, KC_HOME,
-    _______, _______, _______, KC_UP,   _______, _______, KC_INS,  KC_PGDN, KC_PGUP, _______, _______, _______, BL_STEP, _______,          KC_VOLD, KC_END,
-    KC_CAPS, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_MUTE, _______, _______, _______,                   _______,          KC_MUTE,
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,          KC_VOLU, KC_HOME,
+    _______, _______, _______, _______, _______, _______, KC_INS,  KC_PGDN, KC_PGUP, _______, _______, _______, _______, _______,          KC_VOLD, KC_END,
+    KC_CAPS, KC_HOME, _______, KC_DEL,  _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,          _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,                   _______,          KC_MUTE,
     _______, _______, _______,                            _______,                            _______, _______, _______,          KC_MPRV, KC_MPLY, KC_MNXT
   )
 };
